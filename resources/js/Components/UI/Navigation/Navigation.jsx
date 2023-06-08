f6abd6ef0c452733e39f6b578ac3d3be7a283aa6
Jeremiah Ashley <jeremiah.ashley@live.com>
2023-06-08 15:30:45 -0500

    Navigation style fixes

--- resources/js/Components/UI/Navigation/Navigation.jsx
+++ resources/js/Components/UI/Navigation/Navigation.jsx
@@ -34,7 +34,7 @@ export const Navigation = () => {
   return (
     <header
       className={cn(
-        `sticky top-0 z-50 w-full bg-base/75 px-4 backdrop-blur-lg md:px-0`,
+        `sticky top-0 z-50 w-full bg-base/75 px-4 backdrop-blur-md backdrop-brightness-125  backdrop-saturate-150 md:px-0`,
       )}
     >
       <div
@@ -86,53 +86,62 @@ export const Navigation = () => {
         </div>
       </div>
 
-      <Transition
-        show={isOpen}
-        as={Fragment}
-        enter="transition duration-200 ease-out"
-        enterFrom="transform scale-95 opacity-0"
-        enterTo="transform scale-200 opacity-200"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform scale-200 opacity-200"
-        leaveTo="transform scale-95 opacity-0"
-        appear
-      >
-        <Dialog
-          as="nav"
-          open={isOpen}
-          onClose={setIsOpen}
-          className={cn(`fixed left-0 right-0 top-[4.125rem] z-50`)}
-        >
-          <Dialog.Panel
-            className={cn(
-              `flex flex-col items-stretch justify-start space-y-1 border-y border-y-highlightMed/60 bg-base/80 p-4 backdrop-blur-md backdrop-brightness-125  backdrop-saturate-150 md:hidden`,
-            )}
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="nav" onClose={setIsOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="transition-all ease-out duration-100"
+            enterFrom="opacity-0"
+            enterTo="opacity-200"
+            leave="transition-all ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            {navigationLinks.map((link) => (
-              <Link
-                key={link.href}
-                href={link.href}
+            <div className="fixed inset-0 top-[4.125rem] bg-base bg-opacity-50" />
+          </Transition.Child>
+
+          <div className={cn(`fixed left-0 right-0 top-[4.125rem] z-50`)}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300 origin-top"
+              enterFrom="opacity-0 -translate-y-4"
+              enterTo="opacity-100 scale-100 translate-y-0"
+              leave="ease-in duration-100 origin-top"
+              leaveFrom="opacity-100 scale-100 translate-y-0"
+              leaveTo="opacity-0 -translate-y-4"
+            >
+              <Dialog.Panel
                 className={cn(
-                  `hover:shadow-lg/75 group rounded-md border border-transparent px-4 py-2 text-sm shadow-none hover:border-highlightHigh hover:bg-surface md:flex md:flex-row md:items-center md:justify-center`,
-                  currentRouteBeginsWith(link.href)
-                    ? `border-highlightHigh bg-surface`
-                    : ``,
+                  `flex flex-col items-stretch justify-start space-y-1 border-y border-y-highlightMed/60 bg-base/80 p-4 backdrop-blur-md backdrop-brightness-125  backdrop-saturate-150 md:hidden`,
                 )}
-                onClick={() => setIsOpen(() => false)}
               >
-                <span
-                  className={cn(
-                    `font-semibold text-base text-text group-hover:text-love`,
-                    currentRouteBeginsWith(link.href)
-                      ? `font-bold text-love underline decoration-2 underline-offset-2`
-                      : ``,
-                  )}
-                >
-                  {link.name}
-                </span>
-              </Link>
-            ))}
-          </Dialog.Panel>
+                {navigationLinks.map((link) => (
+                  <Link
+                    key={link.href}
+                    href={link.href}
+                    className={cn(
+                      `hover:shadow-lg/75 group rounded-md border border-transparent px-4 py-2 text-sm shadow-none hover:border-highlightHigh hover:bg-surface md:flex md:flex-row md:items-center md:justify-center`,
+                      currentRouteBeginsWith(link.href)
+                        ? `border-highlightHigh bg-surface`
+                        : ``,
+                    )}
+                    onClick={() => setIsOpen(() => false)}
+                  >
+                    <span
+                      className={cn(
+                        `font-semibold text-base text-text group-hover:text-love`,
+                        currentRouteBeginsWith(link.href)
+                          ? `font-bold text-love underline decoration-2 underline-offset-2`
+                          : ``,
+                      )}
+                    >
+                      {link.name}
+                    </span>
+                  </Link>
+                ))}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
         </Dialog>
       </Transition>
     </header>
